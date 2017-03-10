982b54504b39c1b183d2bcffa0f1b99e55da3fc6
Mathis Gerdes <MathisGerdes@gmail.com>
2017-03-10 12:22:57 +0000

    Fix test (i.e. example) file.

--- quantum_circuit/test_gate.py
+++ quantum_circuit/test_gate.py
@@ -1,15 +1,19 @@
 from unittest import TestCase
 from .functional_gates import Gate as FunctionalGate
 from .matrix_gates import Gate as MatrixGate
+import numpy as np
 
 
 s2 = 2**(-.5)
-H = MatrixGate(1, 2, [[s2, s2], [s2, -s2]])
+H = MatrixGate(1, [[s2, s2], [s2, -s2]])
 
 
 class TestGate(TestCase):
     def test_controlled_u(self):
-        for Gate in (FunctionalGate,):
+        print(H * MatrixGate(1, np.transpose(H.matrix)))
+
+        for Gate in (FunctionalGate, MatrixGate):
+            print(Gate)
             gate = Gate.controlled_u(3, H, [1], [0])
 
             print(gate.eval_bs(0))
