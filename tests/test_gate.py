e7f4e60d243aaca17118b09aa482cc16e1f26f1d
Mathis Gerdes <MathisGerdes@gmail.com>
2017-03-18 23:21:55 +0000

    Add simple testcase for gates.

--- tests/test_gate.py
+++ tests/test_gate.py
@@ -1,51 +1,41 @@
-import quantum_circuit
-from quantum_circuit.functional_gates import FunctionalGate
-from quantum_circuit.matrix_gates import MatrixGate
+from quantum_circuit import MatrixGate
+from quantum_circuit import FunctionalGate
+from quantum_circuit import Gate
+from quantum_circuit.gates_library import hadamard
 
-import numpy as np
-from unittest import TestCase
+from tests.testcase import BaseTestCase
 
-
-s2 = 2**(-.5)
-H = MatrixGate(1, [[s2, s2], [s2, -s2]])
+from numpy import complex64
 
 
-class TestGate(TestCase):
-    def test_controlled_u(self):
-        print(H * MatrixGate(1, np.transpose(H.matrix)))
+class TestGate(BaseTestCase):
+    def generic_controlled_u_example(self, gate_type):
+        gate = gate_type.controlled_u(3, hadamard, [1], [0])
 
-        for Gate in (FunctionalGate, MatrixGate):
-            print(Gate)
-            gate = Gate.controlled_u(3, H, [1], [0])
+        # check if type is correct if we explicitly created
+        # a functional / matrix gate
+        if gate_type != Gate:
+            self.assertEqual(type(gate), gate_type)
 
-            print(gate.eval_bs(0))
-            print(gate.eval_bs(1))
-            print(gate.eval_bs(2))
-            print(gate.eval_bs(3))
-            print(gate.eval_bs(4))
-            print(gate.eval_bs(5))
-            print(gate.eval_bs(6))
-            print(gate.eval_bs(7))
+        s2 = complex64(2 ** -.5)
+        # This example is the same as in the docstring of Gate.controlled_u
+        self.assertEqual(gate.eval_bs(0), [1, 0, 0, 0, 0, 0, 0, 0])
+        self.assertEqual(gate.eval_bs(1), [0, s2, 0, s2, 0, 0, 0, 0])
+        self.assertEqual(gate.eval_bs(2), [0, 0, 1, 0, 0, 0, 0, 0])
+        self.assertEqual(gate.eval_bs(3), [0, s2, 0, -s2, 0, 0, 0, 0])
+        self.assertEqual(gate.eval_bs(4), [0, 0, 0, 0, 1, 0, 0, 0])
+        self.assertEqual(gate.eval_bs(5), [0, 0, 0, 0, 0, s2, 0, s2])
+        self.assertEqual(gate.eval_bs(6), [0, 0, 0, 0, 0, 0, 1, 0])
+        self.assertEqual(gate.eval_bs(7), [0, 0, 0, 0, 0, s2, 0, -s2])
 
-            # TODO: actually write a test here (instead of printing)
+    def test_controlled_u_matrix(self):
+        self.generic_controlled_u_example(MatrixGate)
 
+    def test_controlled_u_functional(self):
+        self.generic_controlled_u_example(FunctionalGate)
 
-def test_controlled_u():
-    print(H * MatrixGate(1, np.transpose(H.matrix)))
-
-    for Gate in (FunctionalGate, MatrixGate):
-        print(Gate)
-        gate = Gate.controlled_u(3, H, [1], [0])
-
-        print(gate.eval_bs(0))
-        print(gate.eval_bs(1))
-        print(gate.eval_bs(2))
-        print(gate.eval_bs(3))
-        print(gate.eval_bs(4))
-        print(gate.eval_bs(5))
-        print(gate.eval_bs(6))
-        print(gate.eval_bs(7))
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod(quantum_circuit)
+    def test_controlled_u_default(self):
+        # controlled_u can also be called on the (partially abstract)
+        # Gate class, so the end user could decide to only use Gate
+        # and not bother with Functional/Matrix implementation.
+        self.generic_controlled_u_example(Gate)
--- tests/testcase.py
+++ tests/testcase.py
@@ -2,4 +2,5 @@ from unittest import TestCase
 
 
 class BaseTestCase(TestCase):
+    # use this for basic functionality shared among tests
     pass
