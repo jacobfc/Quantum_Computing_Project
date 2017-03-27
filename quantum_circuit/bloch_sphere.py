bb04712d8ef3b6d31b5c6738ad89635e87aa389c
Stewart Watson <s1432366@sms.ed.ac.uk>
2017-03-27 23:20:52 +0100

    Added proper comments to teleportation and bloch sphere stuff

--- quantum_circuit/bloch_sphere.py
+++ quantum_circuit/bloch_sphere.py
@@ -11,16 +11,31 @@ import numpy as np
 import qutip
 
 def visualise_qubit(s):
+    """
+    This methods uses the qutip module to visualise a qubit's superposition
+    on a bloch sphere.
     
+    This comes from the fact we can write any qubit in the form:
+        
+        |phi> = e^ia*(cos(x/2)|0> + (e^iy)*sin(x/2)|1> )
+        
+    and ignore the global phase e^ia, giving two angles (x,y) that we
+    can use to visualise any qubit uniquely
+    
+    :param s: The state object depicting the qubit we want to visualise
+    """
+    
+    #   Find the global phase to remove
     alpha = cmath.phase(s[0])
 
     A = s[0].real/np.cos(alpha)
     
+    #   find the first angle
     theta = 2*math.acos(A)
     
     sinThetaTwo = np.sin(theta/2.0)
     
-    
+    #   Use this to find the second
     if (np.isclose(sinThetaTwo,s[1])):
         #   This means phi == 0
         phi = 0.0
@@ -29,20 +44,16 @@ def visualise_qubit(s):
         beta = cmath.phase(s[1])
         phi = beta - alpha
     
+    #   Convert these angles to a unit vector using spherical coordinates
     x = np.sin(theta)*np.cos(phi)
     y = np.sin(theta)*np.sin(phi)
     z = np.cos(theta)
     vec = [x, y, z]
     
+    #   Plot this on a bloch sphere
     b = qutip.Bloch()
     b.add_vectors(vec)
     
     b.show()
     b.clear()
     return
-
-def visualise(state, qubit):
-    
-    ### Do some stuff to isolate the qubit into a new State called s ###
-    s = [0,1] ### Remove This ###
-    visualise(s)
--- tests/teleportation_test.py
+++ tests/teleportation_test.py
@@ -14,21 +14,24 @@ import random
 
 def main():
     """
-    |abc>
+    This is a test to outline a simulation of the quantum teleportation concept
     
-    |a> = |0> --<produce state to send>------ ... -----c-H-M----c------------------
-    |b> = |0> ---H-c------------------------- ... -----X---M-c---------------------
-    |c> = |0> -----X------------------------- ... -----------X--Z--<observe state>-
-                  ^
-            ^     |
-            |     Take them any distance away
-            |
+    We produce a random superposition on one qubit (a) and use the circuit 
+    detailed below to transport this qubit's state to another (c).
+    
+    |abc>
+                                                        
+    |a> = |0> --<produce state to send>-- ... -----c-H-M----c------------------
+    |b> = |0> ---H-c--------------------- ... -----X---M-c---------------------
+    |c> = |0> -----X--------------------- ... -----------X--Z--<observe state>-
+                                           ^        ^    alter the phase to get original qubit
+                  ^                        |      Isolate and measure a & b
+                  |                    Take them any distance away
+                  |
             Creating a pair of Bell States
     """
     
-    # Keep the qubits in seperate State objects so we can measure and plot them ?? #
-    
-    # Create a state to teleport
+    #   Create a state to teleport
     s = State.from_basis_state(1,0)
     s = g_lib.hadamard()(s)
     s = g_lib.phase()(s)
@@ -36,15 +39,14 @@ def main():
     
     bloch.visualise_qubit(s)
     
+    #   Store the initial state for comparison
     initial = [s[0], s[1]]
     
-    s_full = State([s[0],0,0,0,s[1],0,0,0])
-    #print(s_full)
     
-    #           |     0             0              0      >
-    #                 ^             ^              ^
-    #                 |             |              |
-    #                 |             |              |
+    #   Now consider the entire system
+    s_full = State([s[0],0,0,0,s[1],0,0,0])
+    
+    # Create the EPR pair
     g_list = [g_lib.identity(), g_lib.hadamard(), g_lib.identity()]
     bell1 = MatrixGate.join_gates(3, g_list)
     bell2 = MatrixGate.controlled_gate(3, g_lib.pauli_x(), [0], [1])
@@ -52,32 +54,44 @@ def main():
     
     s_full = bell(s_full)
 
-    
+
+    #   entangle our desired state with one of the EPR qubits
     invBell1 = MatrixGate.controlled_gate(3, g_lib.pauli_x(), [1], [2])
     g_list = [g_lib.identity(), g_lib.identity(), g_lib.hadamard()]
     invBell2 = MatrixGate.join_gates(3, g_list)
     invBell = invBell2 * invBell1
     
-    
     s_full = invBell(s_full)
     
-    
+    #   Measure the state (this in fact measures the qubit we are wanting to
+    #   keep in a superposition, but the method does not collapse the state
+    #   unlike with a real system. This means we can 'pretend' one of the
+    #   qubits is still in a superposition as follows
     m = s_full.random_measure_bs()
     
+    #   Create a list to hold the amplitudes of the superposed qubit  
     s_new = [0.,0.]
+    
+    #   A series of statements to extract the qubit still in its superposition
+    #   depending on the measurement of the other two bits (as this would 
+    #   normally have collapsed)
+    
+    #   if m == 0 or 1 then our combined state is |000> or |001>
     if m==0 or m==1:
-        do = 0
+        do = 0 #    This tells us the value we need to 'send' in order to
+               #    receive the original superposition (00 in binary)
         
         for i in range(s_full.basis_size):
             if i==0:
-                s_new[0] = s_full[0]
+                s_new[0] = s_full[0]    #   probability of focus qubit being |0>
             elif i==1:
-                s_new[1] = s_full[1]
+                s_new[1] = s_full[1]    #   probability of focus qubit being |1>
             else:
-                s_full[i] = 0.
+                s_full[i] = 0.  #   This manually collapses the state of two of the qubits
     
+    #   if m == 2 or 3 then our combined state is |010> or |011>
     elif m==2 or m==3:
-        do = 1
+        do = 1  #   binary value 01
         
         for i in range(s_full.basis_size):
             if i==2:
@@ -88,7 +102,7 @@ def main():
                 s_full[i] = 0.
     
     elif m==4 or m==5:
-        do = 2
+        do = 2  #   binary 10
         
         for i in range(s_full.basis_size):
             if i==4:
@@ -99,7 +113,7 @@ def main():
                 s_full[i] = 0.
         
     else:
-        do = 3
+        do = 3  #   binary 11
         
         for i in range(s_full.basis_size):
             if i==6:
@@ -109,9 +123,16 @@ def main():
             else:
                 s_full[i] = 0.
     
+    #   Create the new state object that is the focus qubit
     s_new = State(s_new)
+    
+    #   normalise this qubit
     s_new = s_new/(s_new.norm())
     
+    #   This is the section where we 'send' the two classical bits to the
+    #   holder of the focus bit so they can retreive the original state.
+    #   They do this by acting different gates on their qubit depending
+    #   on the classical bits the are given
     if do==0:
         s_new = g_lib.identity()(s_new)
     
@@ -125,6 +146,7 @@ def main():
         s_new = g_lib.pauli_x()(s_new)
         s_new = g_lib.pauli_z()(s_new)
         
+    #   Now, s_new should be exactly equivalent to the initial state
     assert np.isclose(s_new[0],initial[0])
     assert np.isclose(s_new[1],initial[1])
     
