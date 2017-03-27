76cd6391ff07ed13ed856c55e095c060fd1a048c
Mathis Gerdes <MathisGerdes@gmail.com>
2017-03-27 11:49:45 +0100

    Reformat code in tests, add unittest classes.

--- tests/grover_test.py
+++ tests/grover_test.py
@@ -1,16 +1,26 @@
+import quantum_circuit.gates
 from quantum_circuit.gates import State
 import quantum_circuit.gates_library as g_lib
 import numpy as np
 import math
 
-if __name__ == '__main__':
+from tests.testcase import BaseTestCase
+
+
+class TestShor(BaseTestCase):
+    def test_shor(self):
+        # run test and see if errors are thrown
+        main()
+
+
+def main():
     # arbitrary number of qubits
     qubit_count = 4
     basis_size = 1 << qubit_count
     # arbitrary key: 1000 - 1 gives the marked state
     key = 0b0001
     # optimal number of iterations of Grover step
-    n = math.floor(math.pi / (4*math.asin(basis_size ** (-1/2))))
+    n = math.floor(math.pi / (4 * math.asin(basis_size ** (-1 / 2))))
     # initial state
     state = State.from_basis_state(qubit_count, 0)
     # matrix to apply the phase flip
@@ -19,14 +29,18 @@ if __name__ == '__main__':
     diff_gate = g_lib.diffusion_gate(qubit_count)
     # matrix to apply Hadamard to every qubit
     gate_list = [g_lib.hadamard for i in range(qubit_count)]
-    H_n = g_lib.create_gate(qubit_count, gate_list)
+    H_n = quantum_circuit.MatrixGate.join_gates(qubit_count, gate_list)
 
     # create superposition by applying Hadamard to every qubit
     state = State(np.dot(state, H_n.matrix))
     # perform Grover step required number of times
     full = flip_gate * diff_gate
-    for i in range(n-1):
+    for i in range(n - 1):
         full *= flip_gate * diff_gate
     # apply the matrix to get the final state
     final = State(np.dot(state, full.matrix))
-    print (final)
+    print(final)
+
+
+if __name__ == '__main__':
+    main()
